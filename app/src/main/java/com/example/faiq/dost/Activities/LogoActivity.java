6b5c55ee84827d3bd6b90d785cae32c8c4f5e4e3
faiq mustaqeem <faiq.mustaqeem@yahoo.com>
2018-01-20 09:46:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/faiq/dost/Activities/LogoActivity.java
@@ -0,0 +1,15 @@
+package com.example.faiq.dost.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.faiq.dost.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
