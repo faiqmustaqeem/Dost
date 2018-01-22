d570e837383dd202bb327e03b18ad9791ed43b1c
faiq mustaqeem <faiq.mustaqeem@yahoo.com>
2018-01-22 21:03:00 +0500

    commit1

--- app/src/main/java/com/example/faiq/dost/Activities/LoginActivity.java
+++ app/src/main/java/com/example/faiq/dost/Activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,39 +14,35 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LogoActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     Activity activity;
-    @BindView(R.id.create_wallet)
-    Button create_wallet;
-
-    @BindView(R.id.login)
-    Button login;
-
-
+    @BindView(R.id.signin)
+    AppCompatEditText signin;
+    @BindView(R.id.btn_continue)
+    Button btn_continue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logo);
-
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_login);
         activity=this;
-
-
-
-
+        ButterKnife.bind(this);
     }
-    @OnClick ({R.id.login , R.id.create_wallet})
+
+    @OnClick({R.id.signin , R.id.btn_continue})
     public void onClick(View view)
     {
         switch (view.getId())
         {
-            case R.id.login:
-                Intent i=new Intent(activity , LoginActivity.class);
+            case R.id.signin:
+                Intent i=new Intent(activity , SignInActivity.class);
                 startActivity(i);
                 break;
-            case R.id.create_wallet:
+            case R.id.btn_continue:
+                Intent i1=new Intent(activity , MainActivity.class);
+                startActivity(i1);
                 break;
         }
+
     }
 }
---
+++ app/src/main/java/com/example/faiq/dost/Activities/ManualPairingActivity.java
@@ -0,0 +1,15 @@
+package com.example.faiq.dost.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.faiq.dost.R;
+
+public class ManualPairingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_pairing);
+    }
+}
